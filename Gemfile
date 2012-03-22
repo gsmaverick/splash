37be9da6ca67880166f454786d5392eb34f2c1c8
gsmaverick <gavin.schulz@gmail.com>
2012-03-22 16:09:24 -0400

    Added all coffeescript stuff back in

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "http://rubygems.org"
 
 gem 'sinatra'
+gem 'coffee-script'
 gem 'sass'
 gem 'rocco'
 gem 'haml'
--- app.rb
+++ app.rb
@@ -13,9 +13,21 @@ class SassHandler < Sinatra::Base
   end
 end
 
+# Compile and serve coffeescript files
+class CoffeeHandler < Sinatra::Base
+  
+  set :views, File.dirname(__FILE__) + '/js'
+
+  get '/js/*.js'  do
+    filename = params[:splat].first
+    coffee filename.to_sym
+  end
+end
+
 # Application class
 class Splash < Sinatra::Base
   use SassHandler
+  use CoffeeHandler
 
   set :views, File.dirname(__FILE__) + '/views'
 
