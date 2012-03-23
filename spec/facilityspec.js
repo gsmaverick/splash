bb18acc5382521b15c205d8acbca60f768405fe0
gsmaverick <gavin.schulz@gmail.com>
2012-03-23 12:24:28 -0400

    Removed all coffeescript specs and moved back to javascript specs

---
+++ spec/facilityspec.js
@@ -0,0 +1,26 @@
+describe('facility model', function() {
+  it('should be initialized with empty attributes when none are provided', function() {
+    var facility = new Splash.Models.Facility();
+    expect(facility.name()).toBe('');
+    expect(facility.address()).toBe('');
+    expect(facility.city()).toBe('');
+    expect(facility.phone()).toBe('');
+    expect(facility.email()).toBe('');
+  });
+
+  it('should be properly initialized from a JSON string', function() {
+    var facility = new Splash.Models.Facility({
+      name: 'McMaster',
+      address: '1280 Main St.',
+      city: 'Hamilton',
+      phone: '9056485018',
+      email: 'contact@mcmaster.ca'
+    });
+
+    expect(facility.name()).toBe('McMaster');
+    expect(facility.address()).toBe('1280 Main St.');
+    expect(facility.city()).toBe('Hamilton');
+    expect(facility.phone()).toBe('9056485018');
+    expect(facility.email()).toBe('contact@mcmaster.ca');
+  });
+});
