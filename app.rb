f42fe4c06ddd581cc5028d7114c49bd0a8001335
gsmaverick <gavin.schulz@gmail.com>
2012-03-23 12:11:50 -0400

    Changed asset locations for javascripts and test to work with new assetpack bundler

--- app.rb
+++ app.rb
@@ -1,21 +1,32 @@
 require 'rubygems'
 require 'bundler'
 require 'sinatra'
-
-# Compile and serve css files
-class SassHandler < Sinatra::Base
-  
-  set :views, File.dirname(__FILE__) + '/css'
-
-  get '/css/*.css'  do
-    filename = params[:splat].first
-    sass filename.to_sym
-  end
-end
+require 'sinatra/assetpack'
 
 # Application class
 class Splash < Sinatra::Base
-  use SassHandler
+  set :root, File.dirname(__FILE__)
+  register Sinatra::AssetPack
+
+  assets {
+    serve '/js',     from: 'js'        # Optional
+    serve '/css',    from: 'css'       # Optional
+    serve '/images', from: 'images'    # Optional
+
+    # The second parameter defines where the compressed version will be served.
+    # (Note: that parameter is optional, AssetPack will figure it out.)
+    js :app, '/js/app.js', [
+      '/js/vendor/**/*.js',
+      '/js/app/**/*.js'
+    ]
+
+    css :application, '/css/application.css', [
+      '/css/screen.css'
+    ]
+
+    js_compression  :jsmin      # Optional
+    css_compression :sass       # Optional
+  }
 
   set :public_folder, File.dirname(__FILE__) + '/static'
   set :views, File.dirname(__FILE__) + '/views'
